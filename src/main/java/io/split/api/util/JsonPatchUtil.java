45253a1b0e9000b25dd2233b52ec468338353fb3
Fernando Martin <fernando@split.io>
2017-11-15 10:14:24 -0800

    revert back

--- src/main/java/io/split/api/util/JsonPatchUtil.java
+++ src/main/java/io/split/api/util/JsonPatchUtil.java
@@ -2,15 +2,17 @@ package io.split.api.util;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.github.fge.jsonpatch.JsonPatch;
 import com.github.fge.jsonpatch.diff.JsonDiff;
 
+/**
+ * Created on 11/13/17.
+ */
 public class JsonPatchUtil {
     private static ObjectMapper mapper = new ObjectMapper();
 
-    public static <T> JsonPatch createPatch(T source, T target) {
+    public static <T> JsonNode createPatch(T source, T target) {
         JsonNode sourceNode = mapper.valueToTree(source);
         JsonNode targetNode = mapper.valueToTree(target);
-        return JsonDiff.asJsonPatch(sourceNode, targetNode);
+        return JsonDiff.asJson(sourceNode, targetNode);
     }
 }
